c274328fed11df4df5b1fe770a3edfd2cea98a28
Oscar Byström Ericsson <oscar.bystromericsson@gmail.com>
2021-09-28 14:42:41 +0200

    Save, then see if it breaks in a prototype app.

--- Sources/TextFields/WIP/DecimalAdapter.swift
+++ Sources/TextFields/WIP/DecimalAdapter.swift
@@ -5,6 +5,81 @@
 //  Created by Oscar Byström Ericsson on 2021-09-28.
 //
 
-struct DecimalAdapter {
+import Foundation
+
+#warning("WIP")
+struct DecimalAdapter: Adapter {
+    let formatter: NumberFormatter
     
+    init() {
+        #warning("TODO.")
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.generatesDecimalNumbers = true
+        formatter.maximumIntegerDigits = Self.maximumNumberOfDigits
+        formatter.maximumFractionDigits = Self.maximumNumberOfDigits
+        formatter.roundingMode = .down
+        formatter.groupingSeparator = " "
+        self.formatter = formatter
+    }
+    
+    func parse(content: String) throws -> Decimal {
+        let parseable = content.replacingOccurrences(of: formatter.decimalSeparator, with: ".")
+        
+        guard let decimal = Decimal(string: parseable) else {
+            throw ParseFailure()
+        }
+        
+        return decimal
+    }
+    
+    func transcribe(value: Decimal) -> String {
+        String(describing: value).replacingOccurrences(of: ".", with: formatter.decimalSeparator)
+    }
+    
+    func snapshot(content: String) -> Snapshot {
+        var snapshot = Snapshot()
+        
+        guard let decimal: Decimal = try? parse(content: content) else {
+            return snapshot
+        }
+        
+        guard let formatted: String = formatter.string(from: decimal as NSDecimalNumber) else {
+            return snapshot
+        }
+                
+        let content = characters()
+        let spacers = spacers()
+        
+        for character in formatted {
+            if content.contains(character) {
+                snapshot.append(.content(character))
+            } else if spacers.contains(character) {
+                snapshot.append(.spacer(character))
+            }
+        }
+        
+        return snapshot
+    }
+    
+    struct ParseFailure: Error { }
+}
+
+// MARK: - Constants
+
+extension DecimalAdapter {
+    static let maximumNumberOfDigits: Int = 38
+    static let maximumAbsoluteValue = Decimal(string: String(repeating: "9", count: maximumNumberOfDigits))!
+    
+    static let digits = Set<Character>("0123456789")
+    static let decimalSeparator: Character = "."
+    static let minus: Character = "-"
+    
+    func characters() -> Set<Character> {
+        Self.digits.union([Character(formatter.decimalSeparator), Character(formatter.negativePrefix)])
+    }
+    
+    func spacers() -> Set<Character> {
+        Set([Character(formatter.groupingSeparator)])
+    }
 }
