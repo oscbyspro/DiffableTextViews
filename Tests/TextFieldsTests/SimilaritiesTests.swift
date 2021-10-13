a6ef0ee95ea18a73c796163205a2f8bb98df353f
Oscar Byström Ericsson <oscbyspro@gmail.com>
2021-10-13 20:19:21 +0200

    Cleanup: SimilaritiesTests, with SimilaritiesTestsExpectations.

--- Tests/TextFieldsTests/SimilaritiesTests.swift
+++ Tests/TextFieldsTests/SimilaritiesTests.swift
@@ -8,87 +8,123 @@
 import XCTest
 @testable import struct TextFields.Similarities
 
-// MARK: - SimilaritiesTestsOfInspection
+// MARK: - Inspection
 
 final class SimilaritiesTestsOfInspection: XCTestCase {
     typealias Similarities = TextFields.Similarities<[Int], [Int]>
     typealias Options = Similarities.Options
     typealias Inspection = Options.Inspection
+    typealias Expectations = SimilaritiesTestsExpectations<Int>
     
     // MARK: Setup
     
     let lhs = [0, 1, 2, 3, 4]
     let rhs = [0, 2, 4]
+        
+    // MARK: Assertions
     
-    //  MARK: Helpers
-    
-    func similarities(_ inspection: Inspection) -> Similarities {
-        .init(lhs: lhs, rhs: rhs, options: .inspect(inspection))
+    func XCTAssert(_ inspection: Inspection, expectations: () -> Expectations) {
+        let options = Options.inspect(inspection)
+        
+        expectations().validate(Similarities(lhs: lhs, rhs: rhs, options: options))
     }
-    
+        
     // MARK: Tests
     
     func test_each() {
-        let similarities = similarities(.each)
-        
         let prefix = [0]
         let suffix = [4]
         
-        XCTAssert(prefix.elementsEqual(similarities.lhsPrefix()))
-        XCTAssert(suffix.elementsEqual(similarities.lhsSuffix()))
-        XCTAssert(prefix.elementsEqual(similarities.rhsPrefix()))
-        XCTAssert(suffix.elementsEqual(similarities.rhsSuffix()))
+        XCTAssert(.each) {
+            Expectations(
+                lhsPrefix: prefix,
+                lhsSuffix: suffix,
+                rhsPrefix: prefix,
+                rhsSuffix: suffix)
+        }
     }
     
     func test_only() {
-        let similarities = similarities(.only({ $0.isMultiple(of: 2) }))
-
-        XCTAssert(lhs.elementsEqual(similarities.lhsPrefix()))
-        XCTAssert(lhs.elementsEqual(similarities.lhsSuffix()))
-        XCTAssert(rhs.elementsEqual(similarities.rhsPrefix()))
-        XCTAssert(rhs.elementsEqual(similarities.rhsSuffix()))
+        XCTAssert(.only({ $0.isMultiple(of: 2) })) {
+            Expectations(
+                lhsPrefix: lhs,
+                lhsSuffix: lhs,
+                rhsPrefix: rhs,
+                rhsSuffix: rhs)
+        }
     }
 }
 
-// MARK: - SimilaritiesTestsOfProductions
+// MARK: - Production
 
-final class SimilaritiesTestsOfProductions: XCTestCase {
+final class SimilaritiesTestsOfProduction: XCTestCase {
     typealias Similarities = TextFields.Similarities<[Int], [Int]>
     typealias Options = Similarities.Options
     typealias Production = Options.Production
+    typealias Expectations = SimilaritiesTestsExpectations<Int>
     
     // MARK: Setup
     
     let lhs = [1, 0, 0, 0, 1]
     let rhs = [1]
+        
+    // MARK: Assertions
     
-    //  MARK: Helpers
-    
-    func similarities(_ production: Production) -> Similarities {
+    func XCTAssert(_ production: Production, expectations: () -> Expectations) {
         let options = Options
             .inspect(.only({ $0 == 1 }))
             .produce(production)
-        
-        return .init(lhs: lhs, rhs: rhs, options: options)
+                
+        expectations().validate(Similarities(lhs: lhs, rhs: rhs, options: options))
     }
         
     // MARK: Tests
         
     func test_wrapper() {
-        let similarities = similarities(.wrapper)
-
-        XCTAssert(rhs.elementsEqual(similarities.lhsPrefix()))
-        XCTAssert(rhs.elementsEqual(similarities.lhsSuffix()))
-        XCTAssert(rhs.elementsEqual(similarities.rhsPrefix()))
-        XCTAssert(rhs.elementsEqual(similarities.rhsSuffix()))
+        XCTAssert(.wrapper) {
+            Expectations(
+                lhsPrefix: rhs,
+                lhsSuffix: rhs,
+                rhsPrefix: rhs,
+                rhsSuffix: rhs)
+        }
     }
     
     func test_overshoot() {
-        let similarities = similarities(.overshoot)
-        
-        XCTAssert([1, 0, 0, 0].elementsEqual(similarities.lhsPrefix()))
-        XCTAssert([0, 0, 0, 1].elementsEqual(similarities.lhsSuffix()))
-        XCTAssert(         rhs.elementsEqual(similarities.rhsPrefix()))
-        XCTAssert(         rhs.elementsEqual(similarities.rhsSuffix()))
+        XCTAssert(.overshoot) {
+            Expectations(
+                lhsPrefix: [1, 0, 0, 0],
+                lhsSuffix: [0, 0, 0, 1],
+                rhsPrefix: rhs,
+                rhsSuffix: rhs)
+        }
+    }
+}
+
+// MARK: - Expectations
+
+struct SimilaritiesTestsExpectations<Element: Equatable> {
+    let lhsPrefix: AnySequence<Element>?
+    let lhsSuffix: AnySequence<Element>?
+    let rhsPrefix: AnySequence<Element>?
+    let rhsSuffix: AnySequence<Element>?
+    
+    init<S0: Sequence, S1: Sequence, S2: Sequence, S3: Sequence>(
+        lhsPrefix: S0? = nil,
+        lhsSuffix: S1? = nil,
+        rhsPrefix: S2? = nil,
+        rhsSuffix: S3? = nil
+    ) where S0.Element == Element, S1.Element == Element, S2.Element == Element, S3.Element == Element {
+        self.lhsPrefix = lhsPrefix.map(AnySequence.init)
+        self.lhsSuffix = lhsSuffix.map(AnySequence.init)
+        self.rhsPrefix = rhsPrefix.map(AnySequence.init)
+        self.rhsSuffix = rhsSuffix.map(AnySequence.init)
+    }
+    
+    func validate<LHS: BidirectionalCollection, RHS: BidirectionalCollection>(_ similarities: Similarities<LHS, RHS>) where LHS.Element == Element {
+        lhsPrefix.map({ XCTAssert($0.elementsEqual(similarities.lhsPrefix())) })
+        lhsSuffix.map({ XCTAssert($0.elementsEqual(similarities.lhsSuffix())) })
+        rhsPrefix.map({ XCTAssert($0.elementsEqual(similarities.rhsPrefix())) })
+        rhsSuffix.map({ XCTAssert($0.elementsEqual(similarities.rhsSuffix())) })
     }
 }
