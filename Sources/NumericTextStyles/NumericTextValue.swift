2c2af573c0abcca3aaaa6ddfb336271c02a3d645
Oscar Byström Ericsson <oscbyspro@gmail.com>
2021-11-07 18:24:10 +0100

    Cleanup: NumericTextStyles.

--- Sources/NumericTextStyles/NumericTextValue.swift
+++ Sources/NumericTextStyles/NumericTextValue.swift
@@ -15,63 +15,63 @@ public protocol NumericTextValue: Boundable, Precise, Formattable {
 // MARK: - Values: Int
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Int> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Int8> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Int16> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Int32> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Int64> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 // MARK: - Values: UInt
 
 public extension DiffableTextStyle where Self == NumericTextStyle<UInt> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<UInt8> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<UInt16> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<UInt32> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<UInt64> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 // MARK: - Values: Float
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Float16> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Float32> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Float64> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
 
 // MARK: - Values: Decimal
 
 public extension DiffableTextStyle where Self == NumericTextStyle<Decimal> {
-    @inlinable static func numeric(locale: Locale = .autoupdatingCurrent) -> Self { .init(locale: locale) }
+    @inlinable static var numeric: Self { .init() }
 }
