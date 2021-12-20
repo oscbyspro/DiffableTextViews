2f1c22942e8e9ac0d9004c49b73061673d62242e
Oscar Byström Ericsson <oscbyspro@gmail.com>
2021-12-20 08:10:24 +0100

    Cleanup: Formattable.

--- Sources/NumericTextStyles/Protocols/Formattable.swift
+++ Sources/NumericTextStyles/Protocols/Formattable.swift
@@ -12,10 +12,11 @@ import enum Foundation.NumberFormatStyleConfiguration
 // MARK: - Formattable
 
 public protocol Formattable {
+    
+    // MARK: Requirements
+    
     associatedtype FormatStyle: Foundation.FormatStyle where FormatStyle.FormatInput == Self, FormatStyle.FormatOutput == String
     
-    // MARK: Utilities
-        
     /// Interprets a system description this type and returns an instance of it, if the description is valid.
     ///
     /// System description means that fraction separators appear as dots and positive/negative signs appear as single character plus/minus prefixes, etc.
@@ -25,5 +26,6 @@ public protocol Formattable {
     ///
     @inlinable static func value(of description: String) -> Self?
     
+    /// Produces a format style instance configured with the function's parameters.
     @inlinable static func style(locale: Locale, precision: NumberFormatStyleConfiguration.Precision, separator: NumberFormatStyleConfiguration.DecimalSeparatorDisplayStrategy) -> FormatStyle
 }
