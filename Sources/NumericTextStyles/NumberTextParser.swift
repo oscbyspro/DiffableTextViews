26c376a0ef6232772acf1a49166d7b22b3bc3859
Oscar Byström Ericsson <oscbyspro@gmail.com>
2021-12-22 10:03:23 +0100

    Cleanup.

--- Sources/NumericTextStyles/NumberTextParser.swift
+++ Sources/NumericTextStyles/NumberTextParser.swift
@@ -11,39 +11,39 @@ public protocol NumberTextParser: TextParser where Output == NumberText {
     
     // MARK: Requirements
     
-    associatedtype SignParser: TextParser = EmptyTextParser<SignText> where SignParser.Output == SignText
-    @inlinable var sign: SignParser { get }
+    associatedtype SignTextParser: TextParser = EmptyTextParser<SignText> where SignTextParser.Output == SignText
+    @inlinable var sign: SignTextParser { get }
     
-    associatedtype DigitsParser: TextParser = EmptyTextParser<DigitsText> where DigitsParser.Output == DigitsText
-    @inlinable var digits: DigitsParser { get }
+    associatedtype DigitsTextParser: TextParser = EmptyTextParser<DigitsText> where DigitsTextParser.Output == DigitsText
+    @inlinable var digits: DigitsTextParser { get }
     
-    associatedtype SeparatorParser: TextParser = EmptyTextParser<SeparatorText> where SeparatorParser.Output == SeparatorText
-    @inlinable var separator: SeparatorParser { get }
+    associatedtype SeparatorTextParser: TextParser = EmptyTextParser<SeparatorText> where SeparatorTextParser.Output == SeparatorText
+    @inlinable var separator: SeparatorTextParser { get }
 }
 
 // MARK: - NumberTextParser: Sign
 
-public extension NumberTextParser where SignParser == EmptyTextParser<SignText> {
+public extension NumberTextParser where SignTextParser == EmptyTextParser<SignText> {
     
     // MARK: Implementation
     
-    @inlinable @inline(__always) var sign: SignParser { .standard }
+    @inlinable @inline(__always) var sign: SignTextParser { .standard }
 }
 
 // MARK: - NumberTextParser: Digits
 
-public extension NumberTextParser where DigitsParser == EmptyTextParser<DigitsText> {
+public extension NumberTextParser where DigitsTextParser == EmptyTextParser<DigitsText> {
     
     // MARK: Implementation
     
-    @inlinable @inline(__always) var digits: DigitsParser { .standard }
+    @inlinable @inline(__always) var digits: DigitsTextParser { .standard }
 }
 
 // MARK: - NumberTextParser: Separator
 
-public extension NumberTextParser where SeparatorParser == EmptyTextParser<SeparatorText> {
+public extension NumberTextParser where SeparatorTextParser == EmptyTextParser<SeparatorText> {
     
     // MARK: Implementation
     
-    @inlinable @inline(__always) var separator: SeparatorParser { .standard }
+    @inlinable @inline(__always) var separator: SeparatorTextParser { .standard }
 }
