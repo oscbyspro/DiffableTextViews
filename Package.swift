86c86a4c260cacd0edfe44d5075a28f570d87996
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-03 14:47:20 +0100

    Cleanup: Package.swift.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@
 
 import PackageDescription
 
+//=----------------------------------------------------------------------------=
+// MARK: Package
+//=----------------------------------------------------------------------------=
+
 /// TODO: Once done, use stable versions of remote packages or make local copies.
 let package = Package(
     name: "DiffableTextViews",
@@ -19,42 +23,42 @@ let package = Package(
                 "PatternTextStyles"]),
     ],
     dependencies: [
-        // --------------------------------- //
+        //=--------------------------------------=
         // MARK: https://github.com/oscbyspro/
-        // --------------------------------- //
+        //=--------------------------------------=
         .package(name: "Quick", url: "https://github.com/oscbyspro/Quick", .branch("main")),
     ],
     targets: [
-        // --------------------------------- //
+        //=--------------------------------------=
         // MARK: DiffableTextViews
-        // --------------------------------- //
+        //=--------------------------------------=
         .target(
             name: "DiffableTextViews",
             dependencies: ["Quick"]),
         .testTarget(
             name: "DiffableTextViewsTests",
             dependencies: ["DiffableTextViews"]),
-        // --------------------------------- //
+        //=--------------------------------------=
         // MARK: NumericTextStyles
-        // --------------------------------- //
+        //=--------------------------------------=
         .target(
             name: "NumericTextStyles",
             dependencies: ["DiffableTextViews", "Utilities", "Quick"]),
         .testTarget(
             name: "NumericTextStylesTests",
             dependencies: ["NumericTextStyles"]),
-        // --------------------------------- //
+        //=--------------------------------------=
         // MARK: PatternTextStyles
-        // --------------------------------- //
+        //=--------------------------------------=
         .target(
             name: "PatternTextStyles",
             dependencies: ["DiffableTextViews", "Utilities", "Quick"]),
         .testTarget(
             name: "PatternTextStylesTests",
             dependencies: ["PatternTextStyles"]),
-        // --------------------------------- //
+        //=--------------------------------------=
         // MARK: Utilities
-        // --------------------------------- //
+        //=--------------------------------------=
         .target(
             name: "Utilities",
             dependencies: []),
