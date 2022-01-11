c25d514ca8580309053e9ef13b693c7ee1a71304
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-11 11:06:39 +0100

    Work in progress: RegionTests.

---
+++ Tests/NumericTextStylesTests/RegionTests.swift
@@ -0,0 +1,94 @@
+//
+//  RegionTests.swift
+//  
+//
+//  Created by Oscar Byström Ericsson on 2022-01-11.
+//
+
+#if DEBUG
+
+import Foundation
+import XCTest
+
+@testable import NumericTextStyles
+
+//*============================================================================*
+// MARK: * RegionTests
+//*============================================================================*
+
+final class RegionTests: XCTestCase {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: State
+    //=------------------------------------------------------------------------=
+    
+    lazy var regions = Locale
+        .availableIdentifiers
+        .lazy
+        .map(Locale.init)
+        .map(Region.init)
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    func testSigns() {
+        let positive: Int = +1
+        let negative: Int = -1
+        
+        for region in regions {
+            let style = IntegerFormatStyle<Int>
+                .number
+                .locale(region.locale)
+                .sign(strategy: .always())
+            
+            let positives = positive.formatted(style)
+            XCTAssertNotNil(positives.first(where: region.signs.contains))
+
+            let negatives = negative.formatted(style)
+            XCTAssertNotNil(negatives.first(where: region.signs.contains))
+        }
+    }
+    
+    func testDigits() {
+        let number: Int = 1234567890
+        
+        for region in regions {
+            let style = IntegerFormatStyle<Int>
+                .number
+                .locale(region.locale)
+                .grouping(.never)
+            
+            let numbers = number.formatted(style)
+            XCTAssert(numbers.allSatisfy({ region.digits.contains($0) }))
+        }
+    }
+    
+    func testGroupingSeparators() {
+        let number: Int = 1234567890
+        
+        for region in regions {
+            let style = IntegerFormatStyle<Int>
+                .number
+                .locale(region.locale)
+            
+            let nonnumbers = number.formatted(style).filter({ !$0.isNumber })
+            XCTAssertNotNil(nonnumbers.allSatisfy({ $0 == region.groupingSeparator }))
+        }
+    }
+    
+    func testFractionSeparators() {
+        let number: Double = 0.123
+        
+        for region in regions {
+            let style = FloatingPointFormatStyle<Double>
+                .number
+                .locale(region.locale)
+            
+            let nonnumbers = number.formatted(style).filter({ !$0.isNumber })
+            XCTAssertNotNil(nonnumbers.allSatisfy({ $0 == region.fractionSeparator }))
+        }
+    }
+}
+
+#endif
