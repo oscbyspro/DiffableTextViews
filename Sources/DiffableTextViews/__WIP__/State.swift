6c968ff694cc05c30e44aa22041f36c801b39310
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-13 20:41:05 +0100

    Broken.

--- Sources/DiffableTextViews/__WIP__/State.swift
+++ Sources/DiffableTextViews/__WIP__/State.swift
@@ -134,10 +134,31 @@ extension State {
 }
 
 //=----------------------------------------------------------------------------=
-// MARK: State - Move
+// MARK: State - Autocorrect
 //=----------------------------------------------------------------------------=
 
 extension State {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Selection
+    //=------------------------------------------------------------------------=
+    
+    /// It is OK to use intent on both carets at once, because they each have different preferred directions.
+    @inlinable mutating func autocorrect(intent: Direction?) {
+        let upperBound = position(start: selection.upperBound, preference: .backwards, intent: intent)
+        var lowerBound = upperBound
+        
+        if !selection.isEmpty {
+            lowerBound = position(start: selection.lowerBound, preference:  .forwards, intent: intent)
+            lowerBound = min(lowerBound, upperBound)
+        }
+        
+        self.selection = lowerBound ..< upperBound
+    }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Position
+    //=------------------------------------------------------------------------=
     
     @inlinable func position(start: Snapshot.Index, preference: Direction, intent: Direction?) -> Snapshot.Index {
         //=--------------------------------------=
@@ -171,6 +192,13 @@ extension State {
         //=--------------------------------------=
         return position
     }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: State - Move
+//=----------------------------------------------------------------------------=
+
+extension State {
     
     //=------------------------------------------------------------------------=
     // MARK: Direction
