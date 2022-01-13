4353c3771f5d39d380dcf27300e4025e5738963e
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-13 15:25:39 +0100

    Broken.

--- Sources/DiffableTextViews/__WIP__/State.swift
+++ Sources/DiffableTextViews/__WIP__/State.swift
@@ -33,6 +33,39 @@ import Quick
         self.snapshot  = snapshot
         self.selection = selection
     }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: #warning("WIP")
+    //=------------------------------------------------------------------------=
+    
+    #warning("WIP")
+    @inlinable func position(start: Snapshot.Index, preference: Direction, intent: Direction?) -> Snapshot.Index {
+        if peek(at: start).nonlookable(direction: preference) { return start }
+        
+        let direction = intent ?? preference
+        let next = look(start: start, direction: direction)
+        
+        switch direction {
+        case preference: return next
+        case  .forwards: return next != snapshot  .endIndex ? snapshot.index(after:  next) : next
+        case .backwards: return next != snapshot.startIndex ? snapshot.index(before: next) : next
+        }
+    }
+
+    #warning("WIP")
+    @inlinable func look(start: Snapshot.Index, direction: Direction) -> Snapshot.Index {
+        switch direction {
+        case  .forwards: return snapshot[start...].firstIndex(where: \.nonprefixing) ?? snapshot  .endIndex
+        case .backwards: return snapshot[...start] .lastIndex(where: \.nonsuffixing) ?? snapshot.startIndex
+        }
+    }
+    
+    #warning("WIP")
+    @inlinable func peek(at position: Snapshot.Index) -> Peek {
+        let lhs = position != snapshot.startIndex ? snapshot.index(before: position) : nil
+        let rhs = position != snapshot  .endIndex ? position : nil
+        return Peek(lhs: lhs.map({ snapshot[$0] }), rhs: rhs.map({ snapshot[$0] }))
+    }
 }
 
 //=----------------------------------------------------------------------------=
@@ -74,7 +107,7 @@ extension State {
     
     @inlinable mutating func update(snapshot: Snapshot) {
         //=--------------------------------------=
-        // MARK: Bounds
+        // MARK: Single
         //=--------------------------------------=
         let upperBound = Changes(from: self.snapshot[self.selection.upperBound...], to: snapshot[...]).end().next
         var lowerBound = upperBound
@@ -86,14 +119,10 @@ extension State {
             lowerBound = min(lowerBound, upperBound)
         }
         //=--------------------------------------=
-        // MARK: Selection
+        // MARK: Properties
         //=--------------------------------------=
-        let selection = lowerBound..<upperBound
-        //=--------------------------------------=
-        // MARK: Set
-        //=--------------------------------------=
-        self.snapshot  =  snapshot
-        self.selection = selection
+        self.snapshot  = snapshot
+        self.selection = lowerBound..<upperBound
         #warning("Autocorrect...")
     }
 }
@@ -111,74 +140,5 @@ extension State {
     @inlinable mutating func update(selection: Range<Snapshot.Index>) {
         self.selection = selection
         #warning("Autocorrect.")
-    }
-
-    //=------------------------------------------------------------------------=
-    // MARK: Transformations - Intent
-    //=------------------------------------------------------------------------=
-    
-    #warning("Is this")
-    #warning("This affects both, not sure if it should.")
-    @inlinable mutating func update(intent: Direction?) {
-        func position(start: Snapshot.Index, preference: Direction) -> Snapshot.Index {
-            if peek(at: start).nonlookable(direction: preference) { return start }
-            
-            let direction = intent ?? preference
-            let next = look(start: start, direction: direction)
-            
-            switch direction {
-            case preference: return next
-            case  .forwards: return next != snapshot  .endIndex ? snapshot.index(after:  next) : next
-            case .backwards: return next != snapshot.startIndex ? snapshot.index(before: next) : next
-            }
-        }
-        
-        let upperBound = position(start: selection.upperBound, preference: .backwards)
-        var lowerBound = upperBound
-
-        if !selection.isEmpty {
-            lowerBound = position(start: selection.lowerBound, preference:  .forwards)
-            lowerBound = min(lowerBound, upperBound)
-        }
-        
-        self.selection = lowerBound..<upperBound
-        #warning("Autocorrect.")
-    }
-}
-
-//=----------------------------------------------------------------------------=
-// MARK: #warning("Work in progress.")
-//=----------------------------------------------------------------------------=
-
-#warning("WIP")
-extension State {
-    
-    //=------------------------------------------------------------------------=
-    // MARK: WIP
-    //=------------------------------------------------------------------------=
-    
-    @inlinable func look(start: Snapshot.Index, direction: Direction) -> Snapshot.Index {
-        switch direction {
-        case  .forwards: return snapshot[start...].firstIndex(where: \.nonprefixing) ?? snapshot  .endIndex
-        case .backwards: return snapshot[...start] .lastIndex(where: \.nonsuffixing) ?? snapshot.startIndex
-        }
-    }
-}
-
-//=----------------------------------------------------------------------------=
-// MARK: #warning("Work in progress.")
-//=----------------------------------------------------------------------------=
-
-#warning("WIP")
-extension State {
-    
-    //=------------------------------------------------------------------------=
-    // MARK: WIP
-    //=------------------------------------------------------------------------=
-    
-    @inlinable func peek(at position: Snapshot.Index) -> Peek {
-        let lhs = position != snapshot.startIndex ? snapshot.index(before: position) : nil
-        let rhs = position != snapshot  .endIndex ? position : nil
-        return Peek(lhs: lhs.map({ snapshot[$0] }), rhs: rhs.map({ snapshot[$0] }))
     }
 }
