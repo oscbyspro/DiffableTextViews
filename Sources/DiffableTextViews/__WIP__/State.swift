61b55995b271421950aee10ae44a831b4f4fbc40
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-13 21:07:41 +0100

    Broken.

--- Sources/DiffableTextViews/__WIP__/State.swift
+++ Sources/DiffableTextViews/__WIP__/State.swift
@@ -36,6 +36,119 @@ import Quick
 }
 
 //=----------------------------------------------------------------------------=
+// MARK: Update - Snapshot
+//=----------------------------------------------------------------------------=
+
+extension State {
+
+    //=------------------------------------------------------------------------=
+    // MARK: Transformations
+    //=------------------------------------------------------------------------=
+    
+    @inlinable mutating func update(snapshot: Snapshot) {
+        //=--------------------------------------=
+        // MARK: Upper
+        //=--------------------------------------=
+        let upperBound = changesEndIndices(
+            past: self.snapshot[self.selection.upperBound...],
+            next: snapshot[...]).next
+        //=--------------------------------------=
+        // MARK: Lower
+        //=--------------------------------------=
+        var lowerBound = upperBound
+        if !self.selection.isEmpty {
+            lowerBound = changesStartIndices(
+                past: self.snapshot[...self.selection.lowerBound],
+                next: snapshot[...]).next
+            lowerBound = min(lowerBound, upperBound)
+        }
+        //=--------------------------------------=
+        // MARK: Update
+        //=--------------------------------------=
+        self.snapshot  = snapshot
+        self.selection = lowerBound ..< upperBound
+        self.autocorrect(intent: nil)
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: Update - Selection
+//=----------------------------------------------------------------------------=
+
+extension State {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Transformations
+    //=------------------------------------------------------------------------=
+    
+    @inlinable mutating func update(selection: Range<Snapshot.Index>, intent: Direction?) {
+        self.selection = selection
+        self.autocorrect(intent: intent)
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: State - Autocorrect
+//=----------------------------------------------------------------------------=
+
+extension State {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Selection
+    //=------------------------------------------------------------------------=
+    
+    /// It is OK to use intent on both carets at once, because they each have different preferred directions.
+    @inlinable mutating func autocorrect(intent: Direction?) {
+        let upperBound = position(start: selection.upperBound, preference: .backwards, intent: intent)
+        var lowerBound = upperBound
+        
+        if !selection.isEmpty {
+            lowerBound = position(start: selection.lowerBound, preference:  .forwards, intent: intent)
+            lowerBound = min(lowerBound, upperBound)
+        }
+        
+        self.selection = lowerBound ..< upperBound
+    }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Position
+    //=------------------------------------------------------------------------=
+    
+    @inlinable func position(start: Snapshot.Index, preference: Direction, intent: Direction?) -> Snapshot.Index {
+        //=--------------------------------------=
+        // MARK: Position, Direction
+        //=--------------------------------------=
+        var position  = start
+        var direction = direction(at: start) ?? intent ?? preference
+        //=--------------------------------------=
+        // MARK: Correct
+        //=--------------------------------------=
+        loop: while true {
+            //=----------------------------------=
+            // MARK: Move To Next Position
+            //=----------------------------------=
+            position = move(start: position, direction: direction)
+            //=----------------------------------=
+            // MARK: Break Loop Or Jump To Side
+            //=----------------------------------=
+            switch direction {
+            case preference: break loop
+            case  .forwards: position = (position != snapshot  .endIndex) ? snapshot.index(after:  position) : position
+            case .backwards: position = (position != snapshot.startIndex) ? snapshot.index(before: position) : position
+            }
+            //=----------------------------------=
+            // MARK: Repeat In Preferred Direction
+            //=----------------------------------=
+            direction = preference
+        }
+        //=--------------------------------------=
+        // MARK: Return
+        //=--------------------------------------=
+        return position
+    }
+}
+
+//=----------------------------------------------------------------------------=
 // MARK: State - Move
 //=----------------------------------------------------------------------------=
 
@@ -102,119 +215,5 @@ extension State {
         
         if forwards == backwards { return nil }
         return forwards ? .forwards : .backwards
-    }
-}
-
-//=----------------------------------------------------------------------------=
-// MARK: State - Autocorrect
-//=----------------------------------------------------------------------------=
-
-extension State {
-    
-    //=------------------------------------------------------------------------=
-    // MARK: Selection
-    //=------------------------------------------------------------------------=
-    
-    /// It is OK to use intent on both carets at once, because they each have different preferred directions.
-    @inlinable mutating func autocorrect(intent: Direction?) {
-        let upperBound = position(start: selection.upperBound, preference: .backwards, intent: intent)
-        var lowerBound = upperBound
-        
-        if !selection.isEmpty {
-            lowerBound = position(start: selection.lowerBound, preference:  .forwards, intent: intent)
-            lowerBound = min(lowerBound, upperBound)
-        }
-        
-        self.selection = lowerBound ..< upperBound
-    }
-    
-    //=------------------------------------------------------------------------=
-    // MARK: Position
-    //=------------------------------------------------------------------------=
-    
-    @inlinable func position(start: Snapshot.Index, preference: Direction, intent: Direction?) -> Snapshot.Index {
-        //=--------------------------------------=
-        // MARK: Position, Direction
-        //=--------------------------------------=
-        var position  = start
-        var direction = direction(at: start) ?? intent ?? preference
-        //=--------------------------------------=
-        // MARK: Correct
-        //=--------------------------------------=
-        loop: while true {
-            //=----------------------------------=
-            // MARK: Move To Next Position
-            //=----------------------------------=
-            position = move(start: position, direction: direction)
-            //=----------------------------------=
-            // MARK: Break Loop Or Jump To Side
-            //=----------------------------------=
-            switch direction {
-            case preference: break loop
-            case  .forwards: position = (position != snapshot  .endIndex) ? snapshot.index(after:  position) : position
-            case .backwards: position = (position != snapshot.startIndex) ? snapshot.index(before: position) : position
-            }
-            //=----------------------------------=
-            // MARK: Repeat In Preferred Direction
-            //=----------------------------------=
-            direction = preference
-        }
-        //=--------------------------------------=
-        // MARK: Return
-        //=--------------------------------------=
-        return position
-    }
-}
-
-
-//=----------------------------------------------------------------------------=
-// MARK: Update - Snapshot
-//=----------------------------------------------------------------------------=
-
-extension State {
-
-    //=------------------------------------------------------------------------=
-    // MARK: Transformations
-    //=------------------------------------------------------------------------=
-    
-    @inlinable mutating func update(snapshot: Snapshot) {
-        //=--------------------------------------=
-        // MARK: Upper
-        //=--------------------------------------=
-        let upperBound = changesEndIndices(
-            past: self.snapshot[self.selection.upperBound...],
-            next: snapshot[...]).next
-        //=--------------------------------------=
-        // MARK: Lower
-        //=--------------------------------------=
-        var lowerBound = upperBound
-        if !self.selection.isEmpty {
-            lowerBound = changesStartIndices(
-                past: self.snapshot[...self.selection.lowerBound],
-                next: snapshot[...]).next
-            lowerBound = min(lowerBound, upperBound)
-        }
-        //=--------------------------------------=
-        // MARK: Update
-        //=--------------------------------------=
-        self.snapshot  = snapshot
-        self.selection = lowerBound ..< upperBound
-        self.autocorrect(intent: nil)
-    }
-}
-
-//=----------------------------------------------------------------------------=
-// MARK: Update - Selection
-//=----------------------------------------------------------------------------=
-
-extension State {
-    
-    //=------------------------------------------------------------------------=
-    // MARK: Transformations
-    //=------------------------------------------------------------------------=
-    
-    @inlinable mutating func update(selection: Range<Snapshot.Index>, intent: Direction?) {
-        self.selection = selection
-        self.autocorrect(intent: intent)
     }
 }
