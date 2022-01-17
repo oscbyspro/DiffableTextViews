58e1580b26bad993d7d9d80f2a12fdbf4f8cf1c4
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-17 11:56:03 +0100

    Cleanup.

--- Sources/PatternTextStyles/PatternTextStyle.swift
+++ Sources/PatternTextStyles/PatternTextStyle.swift
@@ -68,3 +68,118 @@ public struct PatternTextStyle<Pattern, Value>: DiffableTextStyle, Transformable
 extension PatternTextStyle: UIKitTextStyle { }
 
 #endif
+
+//=----------------------------------------------------------------------------=
+// MARK: PatternTextStyle - Parse
+//=----------------------------------------------------------------------------=
+
+extension PatternTextStyle {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Value
+    //=------------------------------------------------------------------------=
+    
+    @inlinable public func parse(snapshot: Snapshot) throws -> Value {
+        //=--------------------------------------=
+        // MARK: Value
+        //=--------------------------------------=
+        let value = Value(snapshot.lazy.filter({ !$0.contains(.virtual) }).map(\.character))
+        //=--------------------------------------=
+        // MARK: Validate
+        //=--------------------------------------=
+        try validate(value)
+        try predicates.validate(value)
+        //=--------------------------------------=
+        // MARK: Done
+        //=--------------------------------------=
+        return value
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: PatternTextStyle - Snapshot
+//=----------------------------------------------------------------------------=
+
+extension PatternTextStyle {
+
+    //=------------------------------------------------------------------------=
+    // MARK: Value
+    //=------------------------------------------------------------------------=
+    
+    @inlinable public func snapshot(value: Value, mode: Mode) -> Snapshot {
+        var snapshot = Snapshot()
+        //=--------------------------------------=
+        // MARK: Indices
+        //=--------------------------------------=
+        var valueIndex = value.startIndex
+        var patternIndex = pattern.startIndex
+        //=--------------------------------------=
+        // MARK: Body
+        //=--------------------------------------=
+        while patternIndex != pattern.endIndex, valueIndex != value.endIndex {
+            //=----------------------------------=
+            // MARK: Element
+            //=----------------------------------=
+            let patternElement = pattern[patternIndex]
+            pattern.formIndex(after: &patternIndex)
+            //=----------------------------------=
+            // MARK: Match, Insert
+            //=----------------------------------=
+            if patternElement == placeholder {
+                let valueElement = value[valueIndex]
+                value.formIndex(after: &valueIndex)
+                snapshot.append(Symbol(valueElement, as: .content))
+            } else {
+                snapshot.append(Symbol(patternElement, as: .phantom))
+            }
+        }
+        //=--------------------------------------=
+        // MARK: Remainders
+        //=--------------------------------------=
+        if visible {
+            //=----------------------------------=
+            // MARK: Head
+            //=----------------------------------=
+            if valueIndex == value.startIndex, let anchorIndex = pattern[patternIndex...].firstIndex(where: { $0 == placeholder }) {
+                snapshot.append(contentsOf: Snapshot(pattern[patternIndex..<anchorIndex], as: .phantom))
+                snapshot.append(.anchor)
+                patternIndex = anchorIndex
+            }
+            //=----------------------------------=
+            // MARK: Tail
+            //=----------------------------------=
+            snapshot.append(contentsOf: Snapshot(pattern[patternIndex...], as: .phantom))
+        }
+        //=--------------------------------------=
+        // MARK: Done
+        //=--------------------------------------=
+        return snapshot
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: PatternTextStyle - Merge
+//=----------------------------------------------------------------------------=
+
+extension PatternTextStyle {
+        
+    //=------------------------------------------------------------------------=
+    // MARK: Input
+    //=------------------------------------------------------------------------=
+    
+    @inlinable public func merge(snapshot: Snapshot, with input: Input) throws -> Output<Value> {
+        //=--------------------------------------=
+        // MARK: Proposal
+        //=--------------------------------------=
+        var proposal = snapshot
+        proposal.replaceSubrange(input.range, with: input.content)
+        //=--------------------------------------=
+        // MARK: Value
+        //=--------------------------------------=
+        let value = try parse(snapshot: proposal)
+        //=--------------------------------------=
+        // MARK: Snapshot, Output
+        //=--------------------------------------=
+        return Output<Value>(self.snapshot(value: value, mode: .editable), value: value)
+    }
+}
