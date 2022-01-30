4e9c7acba2b5b0bf9028b13ec34f16c40c0e8d48
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-30 11:38:44 +0100

    Cleanup.

--- Package.swift
+++ Package.swift
@@ -52,8 +52,6 @@ let package = Package(
         //=--------------------------------------=
         // MARK: Support
         //=--------------------------------------=
-        .target(
-            name: "Support",
-            dependencies: []),
+        .target(name: "Support", dependencies: []),
     ]
 )
