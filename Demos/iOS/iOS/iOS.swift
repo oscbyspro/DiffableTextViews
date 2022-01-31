c44e7e883cc8425b15fa420c70993c6b30974439
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-01-31 10:39:30 +0100

    iOS demo start.

---
+++ Demos/iOS/iOS/iOS.swift
@@ -0,0 +1,20 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Oscar Byström Ericsson on 2022-01-31.
+//
+
+import SwiftUI
+
+//*============================================================================*
+// MARK: * iOS
+//*============================================================================*
+
+@main struct iOS: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, world!")
+        }
+    }
+}
