4f7bb886fb8c19f4abc64dbcc34178e205bb3d0e
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-02-18 12:06:12 +0100

    Tests.

--- Tests/NumericTextStylesTests/Formats/NumberTests.swift
+++ Tests/NumericTextStylesTests/Formats/NumberTests.swift
@@ -7,4 +7,53 @@
 // See http://www.apache.org/licenses/LICENSE-2.0 for license information.
 //=----------------------------------------------------------------------------=
 
-#warning("WIP")
+#if DEBUG
+
+import XCTest
+@testable import NumericTextStyles
+
+//*============================================================================*
+// MARK: * NumberTests
+//*============================================================================*
+
+final class NumberTests: XCTestCase {
+    typealias Format = Decimal.FormatStyle
+    typealias Style = NumericTextStyle<Format>
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    /// Loops about 1k times.
+    func testAllAvailableLocales() {
+        let value = Decimal(string: "-1234567.89")!
+        //=--------------------------------------=
+        // MARK: Locales
+        //=--------------------------------------=
+        for locale in locales {
+            let style = Style.number.locale(locale)
+            let expectation = Format.number.locale(locale).precision(.fractionLength(0...))
+            //=----------------------------------=
+            // MARK: Testable
+            //=----------------------------------=
+            let commit = style.interpret(value: value)
+            let characters = expectation.format(value)
+            //=----------------------------------=
+            // MARK: Value
+            //=----------------------------------=
+            guard commit.value == value else {
+                XCTFail("\(commit.value) != \(value) ... \((locale))")
+                return
+            }
+            //=----------------------------------=
+            // MARK: Characters
+            //=----------------------------------=
+            guard commit.snapshot.characters == characters else {
+                XCTFail("\(commit.snapshot.characters) != \(characters) ... \((locale))")
+                return
+            }
+        }
+    }
+}
+
+#endif
--- Tests/NumericTextStylesTests/Formats/PercentTests.swift
+++ Tests/NumericTextStylesTests/Formats/PercentTests.swift
@@ -7,4 +7,53 @@
 // See http://www.apache.org/licenses/LICENSE-2.0 for license information.
 //=----------------------------------------------------------------------------=
 
-#warning("WIP")
+#if DEBUG
+
+import XCTest
+@testable import NumericTextStyles
+
+//*============================================================================*
+// MARK: * PercentTests
+//*============================================================================*
+
+final class PercentTests: XCTestCase {
+    typealias Format = Decimal.FormatStyle.Percent
+    typealias Style = NumericTextStyle<Format>
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    /// Loops about 1k times.
+    func testAllAvailableLocales() {
+        let value = Decimal(string: "-1234567.89")!
+        //=--------------------------------------=
+        // MARK: Locales
+        //=--------------------------------------=
+        for locale in locales {
+            let style = Style.percent.locale(locale)
+            let expectation = Format.percent.locale(locale).precision(.fractionLength(0...))
+            //=----------------------------------=
+            // MARK: Testable
+            //=----------------------------------=
+            let commit = style.interpret(value: value)
+            let characters = expectation.format(value)
+            //=----------------------------------=
+            // MARK: Value
+            //=----------------------------------=
+            guard commit.value == value else {
+                XCTFail("\(commit.value) != \(value) ... \((locale))")
+                return
+            }
+            //=----------------------------------=
+            // MARK: Characters
+            //=----------------------------------=
+            guard commit.snapshot.characters == characters else {
+                XCTFail("\(commit.snapshot.characters) != \(characters) ... \((locale))")
+                return
+            }
+        }
+    }
+}
+
+#endif
