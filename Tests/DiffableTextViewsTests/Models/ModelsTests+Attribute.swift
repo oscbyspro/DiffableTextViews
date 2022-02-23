8a1fabbd11b125401984478e324007f640e2af7f
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-02-23 10:33:16 +0100

    Tests: Attribute.

---
+++ Tests/DiffableTextViewsTests/Models/ModelsTests+Attribute.swift
@@ -0,0 +1,45 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the DiffableTextViews open source project.
+//
+// Copyright (c) 2022 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+#if DEBUG
+
+import Support
+import XCTest
+import XCTestSupport
+
+@testable import DiffableTextViews
+
+//*============================================================================*
+// MARK: * ModelsTests x Attribute
+//*============================================================================*
+
+final class ModelsTestsXAttribute: Tests {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: State
+    //=------------------------------------------------------------------------=
+    
+    let attributes: [Attribute] = [.virtual, .insertable, .removable, .passthrough]
+
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    func testContentIsEmpty() {
+        XCTAssert(Attribute.content.isEmpty)
+    }
+    
+    func testEachAttributeIsUnique() {
+        for attribute in attributes {
+            XCTAssertEqual(attributes.count(where: { $0 == attribute }), 1)
+        }
+    }
+}
+
+#endif
