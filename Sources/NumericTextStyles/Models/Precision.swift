c0d20ace980bffe63ce84d51ac16bd794fbb01c5
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-02-24 13:57:25 +0100

    Improved: NumericTextStyle/Precision initialization (removed redundant range interpretations).

--- Sources/NumericTextStyles/Models/Precision.swift
+++ Sources/NumericTextStyles/Models/Precision.swift
@@ -25,43 +25,67 @@ public struct Precision<Value: NumericTextValue>: Equatable {
     // MARK: Initializers
     //=------------------------------------------------------------------------=
     
+    @inlinable init(lower: Count, upper: Count) {
+        self.lower = lower
+        self.upper = upper
+    }
+
+    //=------------------------------------------------------------------------=
+    // MARK: Initializers - Indirect
+    //=------------------------------------------------------------------------=
+    
     @inlinable init() {
-        self.init(integer: Self.limits(\.integer), fraction: Self.limits(\.fraction))
+        self = .unchecked()
     }
     
-    @inlinable init<R>(integer:  R) where
-    R: RangeExpression, R.Bound == Int {
-        self.init(integer: integer, fraction: Self.limits(\.fraction))
+    @inlinable init<R>(integer:  R) where R: RangeExpression, R.Bound == Int {
+        self = .unchecked(integer:  Self.interpret(integer,  as: \.integer ))
     }
     
-    @inlinable init<R>(fraction: R) where
-    R: RangeExpression, R.Bound == Int {
-        self.init(integer: Self.limits(\.integer), fraction: fraction)
+    @inlinable init<R>(fraction: R) where R: RangeExpression, R.Bound == Int {
+        self = .unchecked(fraction: Self.interpret(fraction, as: \.fraction))
     }
     
     @inlinable init<R0, R1>(integer: R0, fraction: R1) where
     R0: RangeExpression, R0.Bound == Int, R1: RangeExpression, R1.Bound == Int {
-        let integer  = Namespace.interpret(integer,  in: Self.limits(\.integer ))
-        let fraction = Namespace.interpret(fraction, in: Self.limits(\.fraction))
-        //=--------------------------------------=
-        // MARK: Initialize
-        //=--------------------------------------=
-        self.lower = Count(
-        value: Namespace.minimum.value,
-        integer:   integer .lowerBound,
-        fraction:  fraction.lowerBound)
-        self.upper = Count(
-        value:   Value.precision.value,
-        integer:   integer .upperBound,
-        fraction:  fraction.upperBound)
+        self = .unchecked(integer:  Self.interpret(integer,  as: \.integer ),
+                          fraction: Self.interpret(fraction, as: \.fraction))
     }
     
     //=------------------------------------------------------------------------=
     // MARK: Initializers - Helpers
     //=------------------------------------------------------------------------=
     
+    @inlinable static func unchecked(
+    integer:  ClosedRange<Int> = limits(\.integer ),
+    fraction: ClosedRange<Int> = limits(\.fraction)) -> Self {
+        //=--------------------------------------=
+        // MARK: Lower
+        //=--------------------------------------=
+        let lower = Count(
+        value: Namespace.minimum.value,
+        integer:   integer .lowerBound,
+        fraction:  fraction.lowerBound)
+        //=--------------------------------------=
+        // MARK: Upper
+        //=--------------------------------------=
+        let upper = Count(
+        value:   Value.precision.value,
+        integer:   integer .upperBound,
+        fraction:  fraction.upperBound)
+        //=--------------------------------------=
+        // MARK: Intantiate
+        //=--------------------------------------=
+        return Self(lower: lower, upper: upper)
+    }
+    
     @inlinable static func limits(_ component: (Count) -> Int) -> ClosedRange<Int> {
         component(Namespace.minimum)...component(Value.precision)
+    }
+    
+    @inlinable static func interpret<R>(_ expression: R,
+    as component: (Count) -> Int) -> ClosedRange<Int> where R: RangeExpression, R.Bound == Int {
+        Namespace.interpret(expression, in: limits(component))
     }
 }
 
