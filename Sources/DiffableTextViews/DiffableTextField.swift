be700f76d76e18fe850bd31dab25153fd0e68255
Oscar Byström Ericsson <oscbyspro@gmail.com>
2022-03-03 06:58:07 +0100

    Cleanup: DiffableTextField.

--- Sources/DiffableTextViews/DiffableTextField.swift
+++ Sources/DiffableTextViews/DiffableTextField.swift
@@ -255,4 +255,78 @@ public struct DiffableTextField<Style: UIKitDiffableTextStyle>: UIViewRepresenta
     }
 }
 
+//*============================================================================*
+// MARK: * DiffableTextField x ID
+//*============================================================================*
+
+public struct DiffableTextFieldID { public static let diffableTextField = Self() }
+
+//*============================================================================*
+// MARK: * DiffableTextField x Environment
+//*============================================================================*
+
+@usableFromInline struct DiffableTextField_OnSetup: EnvironmentKey {
+    @usableFromInline static let defaultValue: (ProxyTextField) -> Void = { _ in }
+}
+
+@usableFromInline enum DiffableTextField_OnUpdate: EnvironmentKey {
+    @usableFromInline static let defaultValue: (ProxyTextField) -> Void = { _ in }
+}
+
+@usableFromInline enum DiffableTextField_OnSubmit: EnvironmentKey {
+    @usableFromInline static let defaultValue: (ProxyTextField) -> Void = { _ in }
+}
+
+//*============================================================================*
+// MARK: * DiffableTextField x Environment x Values
+//*============================================================================*
+
+extension EnvironmentValues {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Accessors
+    //=------------------------------------------------------------------------=
+    
+    @inlinable var diffableTextField_onSetup:  (ProxyTextField) -> Void {
+        get { self[DiffableTextField_OnSetup .self] }
+        set { self[DiffableTextField_OnSetup .self] = newValue }
+    }
+    
+    @inlinable var diffableTextField_onUpdate: (ProxyTextField) -> Void {
+        get { self[DiffableTextField_OnUpdate.self] }
+        set { self[DiffableTextField_OnUpdate.self] = newValue }
+    }
+
+    @inlinable var diffableTextField_onSubmit: (ProxyTextField) -> Void {
+        get { self[DiffableTextField_OnSubmit.self] }
+        set { self[DiffableTextField_OnSubmit.self] = newValue }
+    }
+}
+
+//*============================================================================*
+// MARK: * DiffableTextField x Environment x View
+//*============================================================================*
+
+public extension View {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Transformations
+    //=------------------------------------------------------------------------=
+    
+    @inlinable func onSetup(of view: DiffableTextFieldID,
+    _ action: @escaping (ProxyTextField) -> Void) -> some View {
+        environment(\.diffableTextField_onSetup, action)
+    }
+    
+    @inlinable func onUpdate(of view: DiffableTextFieldID,
+    _ action: @escaping (ProxyTextField) -> Void) -> some View {
+        environment(\.diffableTextField_onUpdate, action)
+    }
+    
+    @inlinable func onSubmit(of view: DiffableTextFieldID,
+    _ action: @escaping (ProxyTextField) -> Void) -> some View {
+        environment(\.diffableTextField_onSubmit, action)
+    }
+}
+
 #endif
