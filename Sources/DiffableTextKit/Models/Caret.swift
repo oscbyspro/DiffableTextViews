oscbyspro/DiffableTextViews

commit 04b96cc1bc95cd8e8c7ead84b9f73455a1259004

Cleanup and idle update optimization.

Sources/DiffableTextKit/Models/Selection.swift
@@ -16,6 +16,7 @@
 /// Equal carets represents an upper caret.
 ///
 @usableFromInline struct Selection<Caret: Comparable>: Equatable {
+    @usableFromInline typealias Detached = DiffableTextKit.Detached<Caret>
     
     //=------------------------------------------------------------------------=
     // MARK: State
@@ -39,15 +40,7 @@
     @inlinable init(_ range: Range<Caret>) {
         self.init(unchecked: (range.lowerBound, range.upperBound))
     }
-    
-    //=------------------------------------------------------------------------=
-    // MARK: Initializers
-    //=------------------------------------------------------------------------=
-    
-    @inlinable static func max<T>(_ collection: T) -> Self where T: Collection, T.Index == Caret {
-        Self(unchecked: (collection.startIndex, collection.endIndex))
-    }
-    
+        
     //=------------------------------------------------------------------------=
     // MARK: Accessors
     //=------------------------------------------------------------------------=
@@ -68,8 +61,8 @@
     // MARK: Transformations
     //=------------------------------------------------------------------------=
     
-    @inlinable func map<T>(caret: (Caret) -> T) -> Selection<T> {
-        return map(lower:  caret, upper: caret)
+    @inlinable func map<T>(_ caret: (Caret) -> T) -> Selection<T> {
+        return map(lower: caret, upper: caret)
     }
     
     @inlinable func map<T>(lower: (Caret) -> T, upper: (Caret) -> T) -> Selection<T> {
@@ -84,5 +77,41 @@
         // Return
         //=--------------------------------------=
         return Selection<T>(unchecked: (min, max))
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: + Detached
+//=----------------------------------------------------------------------------=
+
+extension Selection {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Transformations
+    //=------------------------------------------------------------------------=
+    
+    @inlinable func detached(_ momentums: Momentums) -> Selection<Detached> {
+        Selection<Detached>(unchecked: (
+        Detached.lower(lower,momentum: momentums.lower),
+        Detached.upper(upper,momentum: momentums.upper)))
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: + Snapshot
+//=----------------------------------------------------------------------------=
+
+extension Selection where Caret == Index {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Initializers
+    //=------------------------------------------------------------------------=
+    
+    @inlinable static func max(_ snapshot:  Snapshot) -> Self {
+        Self(unchecked:(snapshot.startIndex,snapshot.endIndex))
+    }
+    
+    @inlinable static func standard(_ snapshot: Snapshot) -> Self {
+        Selection<Detached>(.upper(snapshot.endIndex)).map(snapshot.caret)
     }
 }

Sources/DiffableTextKit/Models/Snapshot.swift
@@ -454,9 +454,7 @@ extension Snapshot {
     // MARK: Search
     //=------------------------------------------------------------------------=
     
-    /// Returns the preferred caret if it exists, returns endIndex otherwise.
-    @inlinable func caret(from index: Index, towards direction: Direction?,
-    preferring preference: Direction) -> Index {
+    @inlinable func caret(_ detached: Detached<Index>) -> Index {
         //=--------------------------------------=
         // Anchor
         //=--------------------------------------=
@@ -465,35 +463,35 @@ extension Snapshot {
         // Inspect Initial Index
         //=--------------------------------------=
         if let peek = peek(
-        from: index,
-        towards: preference),
-        nonpassthrough(at: peek) { return index }
+        from: detached.position,
+        towards: detached.affinity),
+        nonpassthrough(at: peek) { return detached.position }
         //=--------------------------------------=
         // Direction
         //=--------------------------------------=
-        var direction = direction ?? preference
+        var direction = detached.momentum ?? detached.affinity
         //=--------------------------------------=
         // Search In Direction
         //=--------------------------------------=
-        if let caret = caret(
-        from: index,
+        if let index = self.caret(
+        from: detached.position,
         towards: direction,
-        jumping: direction == preference ? .to : .through,
-        targeting: nonpassthrough) { return caret }
+        jumping: direction == detached.affinity ? .to : .through,
+        targeting: nonpassthrough) { return index }
         //=--------------------------------------=
         // Search In Opposite Direction
         //=--------------------------------------=
         direction.reverse()
         
-        if let caret = caret(
-        from: index,
+        if let index = self.caret(
+        from: detached.position,
         towards: direction,
         jumping: Jump.to,
-        targeting: nonpassthrough) { return caret }
+        targeting: nonpassthrough) { return index }
         //=--------------------------------------=
-        // Use Default Index On Caret Not Found
+        // Return Preference On Caret Not Found
         //=--------------------------------------=
-        return self.defaultIndex
+        return detached.affinity == .backwards ? startIndex : endIndex
     }
     
     //=------------------------------------------------------------------------=

Sources/DiffableTextKit/Helpers/Momentums.swift
@@ -35,7 +35,7 @@
         self.upper = upper
     }
     
-    @inlinable init<T>(from start: Selection<T>, to end: Selection<T>) {
+    @inlinable init<T>(from start: Selection<T>,  to  end: Selection<T>) {
         self.lower = Direction(from: start.lower, to: end.lower)
         self.upper = Direction(from: start.upper, to: end.upper)
     }

Sources/DiffableTextKit/Models/Context.swift
@@ -46,7 +46,7 @@ public struct Context<Style: DiffableTextStyle> {
         // Unique
         //=--------------------------------------=
         if !isKnownUniquelyReferenced(&storage) {
-            self.storage = Storage(status,layout)
+            self.storage = Storage(status, layout, backup)
         }
         //=--------------------------------------=
         // Update
@@ -58,10 +58,10 @@ public struct Context<Style: DiffableTextStyle> {
         //=--------------------------------------=
         // Active
         //=--------------------------------------=
-        if other.focus == true {
-            self.write {
-                $0.status = other.status
-                $0.layout.merge(snapshot: other.layout.snapshot)
+        if self.layout != nil, other.layout != nil {
+            self.write { storage in
+                storage.status = other.status
+                storage.layout!.merge(snapshot: other.layout!.snapshot)
             }
         //=--------------------------------------=
         // Inactive
@@ -80,15 +80,22 @@ public struct Context<Style: DiffableTextStyle> {
         //=--------------------------------------------------------------------=
         
         @usableFromInline var status: Status
-        @usableFromInline var layout: Layout
+        @usableFromInline var layout: Layout!
+        @usableFromInline var backup: String!
         
         //=--------------------------------------------------------------------=
         // MARK: Initializers
         //=--------------------------------------------------------------------=
         
-        @inlinable init(_ status: Status, _ layout: Layout) {
+        @inlinable init(_ status: Status, _ layout: Layout?, _ backup: String?) {
             self.status = status
             self.layout = layout
+            self.backup = backup
+            //=----------------------------------=
+            // Invariants
+            //=----------------------------------=
+            assert((status.focus == true) == (layout != nil))
+            assert((status.focus == true) == (backup == nil))
         }
     }
 }
@@ -109,8 +116,13 @@ extension Context {
     }
     
     @inlinable @inline(__always)
-    var layout: Layout {
+    var layout: Layout? {
         storage.layout
+    }
+    
+    @inlinable @inline(__always)
+    var backup: String? {
+        storage.backup
     }
     
     //=------------------------------------------------------------------------=
@@ -138,17 +150,18 @@ extension Context {
     
     @inlinable @inline(__always)
     public var text: String {
-        layout.snapshot.characters
+        layout?.snapshot.characters ?? backup!
     }
     
     @inlinable @inline(__always)
     public var selection: Range<String.Index> {
-        layout.selection.map(caret: \.character).range
+        layout?.selection.map( \.character).range ??
+        backup!.startIndex ..< backup!.startIndex
     }
     
     @inlinable @inline(__always)
     public func selection<T>(as type: T.Type = T.self) -> Range<Offset<T>> {
-        layout.snapshot.distances(to: layout.selection.range)
+        layout?.selection() ?? .zero ..< .zero
     }
 }
 
@@ -173,14 +186,12 @@ extension Context {
             changes?.pointee.formUnion(.value(commit.value != status.value))
             
             status.value = commit.value
-            self.storage = Storage(status, Layout(commit.snapshot))
-            //=--------------------------------------=
-            // Inactive
-            //=--------------------------------------=
+            self.storage = Storage(status, Layout(commit.snapshot), nil)
+        //=--------------------------------------=
+        // Inactive
+        //=--------------------------------------=
         } else {
-            let characters = status.format(with: &cache)
-            let snapshot = Snapshot(characters,as: .phantom)
-            self.storage = Storage(status, Layout(snapshot))
+            self.storage = Storage(status, nil, status.format(with: &cache))
         }
     }
     
@@ -229,25 +240,29 @@ extension Context {
     /// Call this on changes to text.
     @inlinable public mutating func merge(_   characters: String, in range:
     Range<Offset<some Encoding>>, with cache: inout Cache) throws -> Update {
-        let proposal = Proposal(layout.snapshot,
-        with: Snapshot(characters,as: .content),
-        in: layout.snapshot.indices(at:  range))
-        //=----------------------------------=
+        if layout == nil { return [] }
+        //=--------------------------------------=
+        // Values
+        //=--------------------------------------=
+        let proposal = Proposal(layout!.snapshot,
+        with: Snapshot(characters, as: .content),
+        in: layout!.snapshot.indices(at:  range))
+        //=--------------------------------------=
         // Commit
-        //=----------------------------------=
+        //=--------------------------------------=
         let commit = try status.resolve(proposal, with: &cache)
         var update = Update.value(value != commit.value)
-        //=----------------------------------=
+        //=--------------------------------------=
         // Update
-        //=----------------------------------=
-        self.write {
-            $0.status.value = commit.value
-            $0.layout.selection.collapse()
-            $0.layout.merge(snapshot: commit.snapshot)
+        //=--------------------------------------=
+        self.write { storage in
+            storage.status.value = commit.value
+            storage.layout.selection.collapse()
+            storage.layout.merge(snapshot: commit.snapshot)
         }
-        //=----------------------------------=
+        //=--------------------------------------=
         // Return
-        //=----------------------------------=
+        //=--------------------------------------=
         update.formUnion(.text)
         update.formUnion(.selection(focus == true))
         return update
@@ -260,18 +275,22 @@ extension Context {
     /// Call this on changes to selection.
     @inlinable public mutating func merge(
     selection: Range<Offset<some Encoding>>, momentums: Bool) -> Update {
-        let selection = Selection(layout.snapshot.indices(at: selection))
+        if layout == nil { return [] }
+        //=--------------------------------------=
+        // Values
+        //=--------------------------------------=
+        let selection = Selection(layout!.snapshot.indices(at: selection))
         //=--------------------------------------=
         // Update
         //=--------------------------------------=
-        self.write {
-            $0.layout.merge(
+        self.write { storage in
+            storage.layout!.merge(
             selection: selection,
             momentums: momentums)
         }
         //=--------------------------------------=
         // Return
         //=--------------------------------------=
-        return Update.selection(layout.selection != selection)
+        return .selection(layout!.selection != selection)
     }
 }

Sources/DiffableTextKit/Models/Detached.swift
@@ -1,0 +1,57 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the DiffableTextViews open source project.
+//
+// Copyright (c) 2022 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+//*============================================================================*
+// MARK: * Detached
+//*============================================================================*
+
+@usableFromInline struct Detached<Position: Comparable>: Comparable {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: State
+    //=------------------------------------------------------------------------=
+    
+    @usableFromInline let position: Position
+    @usableFromInline let affinity: Direction
+    @usableFromInline let momentum: Direction?
+
+    //=------------------------------------------------------------------------=
+    // MARK: Initializers
+    //=------------------------------------------------------------------------=
+    
+    @inlinable init(position: Position, momentum: Direction?, affinity: Direction) {
+        self.position = position
+        self.momentum = momentum
+        self.affinity = affinity
+    }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Initializers
+    //=------------------------------------------------------------------------=
+    
+    @inlinable static func lower(_ position: Position, momentum: Direction? = nil) -> Self {
+        .init(position: position,  momentum: momentum, affinity: .forwards)
+    }
+    
+    @inlinable static func upper(_ position: Position, momentum: Direction? = nil) -> Self {
+        .init(position: position,  momentum: momentum, affinity:.backwards)
+    }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Utilities
+    //=------------------------------------------------------------------------=
+    
+    @inlinable static func < (lhs: Self, rhs: Self) -> Bool {
+        lhs.position < rhs.position
+    }
+    
+    @inlinable static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.position < rhs.position
+    }
+}

Sources/DiffableTextKit/Models/Layout.swift
@@ -31,7 +31,15 @@
     //=------------------------------------------------------------------------=
     
     @inlinable init(_ snapshot: Snapshot) {
-        self.snapshot = snapshot; self.selection = Selection(snapshot.defaultIndex)
+        self.snapshot = snapshot; self.selection = .standard(snapshot)
+    }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Accessors
+    //=------------------------------------------------------------------------=
+    
+    @inlinable public func selection<T>(as type: T.Type = T.self) -> Range<Offset<T>> {
+        snapshot.distances(to:  selection.range)
     }
     
     //=------------------------------------------------------------------------=
@@ -51,17 +59,18 @@
     
     /// Use this on changes to selection.
     @inlinable mutating func merge(selection: Selection, momentums: Bool) {
-        let  momentums = momentums ? Momentums(from: self.selection, to: selection) : .none
-        self.selection = selection
         //=--------------------------------------=
         // Accept Max Selection
         //=--------------------------------------=
-        if selection == .max(snapshot) { return }
+        if selection == .max(snapshot) {
+            self.selection = selection; return
+        }
         //=--------------------------------------=
         // Autocorrect
         //=--------------------------------------=
-        self.selection = selection.map(
-        lower: { snapshot.caret(from: $0, towards: momentums.lower, preferring:  .forwards) },
-        upper: { snapshot.caret(from: $0, towards: momentums.upper, preferring: .backwards) })
+        let  momentums = !momentums ? Momentums.none :
+        Momentums(from:  self.selection, to:selection)
+        let  selection = selection.detached(momentums)
+        self.selection = selection.map(snapshot.caret)
     }
 }

commit bec6d8474f7331ea9dccdc186e3ac9f605166ceb

Cleanup. Fixed bug introduced by: 04b96cc1bc95cd8e8c7ead84b9f73455a1259004.

Sources/DiffableTextKit/Models/Caret.swift
@@ -11,6 +11,10 @@
 // MARK: * Caret
 //*============================================================================*
 
+/// A position with special characteristics.
+///
+/// - It SHOULD only compare positions.
+///
 public protocol Caret<Position>: Comparable {
     associatedtype Position: Comparable
     
@@ -32,8 +36,12 @@ public extension Caret {
     // MARK: Uilities
     //=------------------------------------------------------------------------=
     
-    @inlinable static func < (lhs: Self, rhs: Self) -> Bool {
-        lhs.position < rhs.position
+    @inlinable static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.position == rhs.position
+    }
+    
+    @inlinable static func <  (lhs: Self, rhs: Self) -> Bool {
+        lhs.position <  rhs.position
     }
 }
 

Sources/DiffableTextKit/Models/Selection.swift
@@ -35,12 +35,12 @@ public struct Selection<Position: Comparable>: Equatable {
         self.upper = Upper(unchecked.upper)
     }
     
-    @inlinable init(_ caret: Position) {
-        self.init(unchecked: (caret, caret))
+    @inlinable init(_ position: Position) {
+        self.init(unchecked: (position, position))
     }
     
-    @inlinable init(_ range: Range<Position>) {
-        self.init(unchecked: (range.lowerBound, range.upperBound))
+    @inlinable init(_ positions: Range<Position>) {
+        self.init(unchecked: (positions.lowerBound, positions.upperBound))
     }
     
     //=------------------------------------------------------------------------=

Sources/DiffableTextKit/Models/Snapshot.swift
@@ -461,7 +461,7 @@ extension Snapshot {
         //=--------------------------------------=
         if let anchor { return anchor }
         //=--------------------------------------=
-        // Inspect Initial Index
+        // Inspect Initial Position
         //=--------------------------------------=
         if let peek = peek(
         from: detached.position,